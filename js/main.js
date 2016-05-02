f9ecabc00d6176e79b38a731b5aa6f90b2674fbc
drch <davrchouse@gmail.com>
2016-05-01 20:31:53 -0700

    js for rest of project box text & added links to footer

--- js/main.js
+++ js/main.js
@@ -1,34 +1,12 @@
-// $(function() {
-//     var state = true;
-//     $( "#button" ).click(function() {
-//       if ( state ) {
-//         $( "#resume" ).animate({
-//           width: 500
-//         }, 1000 );
-//       } else {
-//         $( "#resume" ).animate({
-//           width: 240
-//         }, 1000 );
-//       }
-//       state = !state;
-//     });
-//   });
+$(document).ready(function(){
+        $("div.description").hide();
+ });
 
-// $(function() {
-//     var state = true;
-//     $("#button").click(function() {
-//         if ( state ) {  
-//           $("p").css("display", "none");
-//         } else {
-//           $("p").css("display", "block");
-//         }                             
-//         state = !state;
-//       });
-//     });
 
 $(function() {
     var state = true;
-    $("#button").click(function() {
+
+    $("#resume_button").click(function() {
         if ( state ) {  
           $("#resume").show(100);
         } else {
@@ -39,44 +17,70 @@ $(function() {
     });
 
 
+$(function() {
+    var state = true;
+
+    $("#map_button").click(function() {
+        if ( state ) {  
+          $("#map").show(100);
+        } else {
+          $("#map").hide(100);
+        }                             
+        state = !state;
+      });
+    });
+
+$(function() {
+    var state = true;
+
+    $("#game_button").click(function() {
+        if ( state ) {  
+          $("#game").show(100);
+        } else {
+          $("#game").hide(100);
+        }                             
+        state = !state;
+      });
+    });
 
 
-// $(document).ready(function(){
-//     $("#button").click(function(){
-//         $("p").css("display", "none");
-//     });
-// });
+$(function() {
+    var state = true;
+
+    $("#feeder_button").click(function() {
+        if ( state ) {  
+          $("#feeder").show(100);
+        } else {
+          $("#feeder").hide(100);
+        }                             
+        state = !state;
+      });
+    });
+
+$(function() {
+    var state = true;
+
+    $("#web_button").click(function() {
+        if ( state ) {  
+          $("#web").show(100);
+        } else {
+          $("#web").hide(100);
+        }                             
+        state = !state;
+      });
+    });
+
+$(function() {
+    var state = true;
+
+    $("#heart_button").click(function() {
+        if ( state ) {  
+          $("#heart").show(100);
+        } else {
+          $("#heart").hide(100);
+        }                             
+        state = !state;
+      });
+    });
 
 
-
- // $(function() {
- //    // run the currently selected effect
- //    function runEffect() {
- //      // get effect type from
- //      var selectedEffect = blind;
- 
- //      // most effect types need no options passed by default
- //      var options = {};
- //      // some effects have required parameters
- //      // if ( selectedEffect === "scale" ) {
- //      //   options = { percent: 0 };
- //      // } else if ( selectedEffect === "size" ) {
- //      //   options = { to: { width: 200, height: 60 } };
- //      // }
- 
- //      // run the effect
- //      $( "#resume" ).hide( selectedEffect, options, 1000, callback );
- //    };
- 
- //    // callback function to bring a hidden box back
- //    function callback() {
- //      setTimeout(function() {
- //        $( "#resume" ).removeAttr( "style" ).hide().fadeIn();
- //      }, 1000 );
- //    };
- 
- //    // set effect from select menu value
- //    $( "#button" ).click(function() {
- //      runEffect();
- //    });
- //  });
